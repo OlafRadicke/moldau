320cf56559cb9ca92eae79d21c921b81a7dd42e6
Olaf Radicke <briefkasten@olaf-radicke.de>
2011-03-12 11:06:00 +0100

    add dir and file

---
+++ src/moldau.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+
+
+
+
+
+
+
+class MainClass():
+
+    def Start(selfself):
+        print "Start..."
+        
+    def Stop(selfself):
+        print "...Stop"
+    
+
+print "läuft..."
+mc = MainClass()
+mc.Start()
+mc.Stop()
