5f3e259bfd18df77be65b3281d7c9d7c7b7586f5
Olaf Radicke <briefkasten@olaf-radicke.de>
2011-03-13 17:08:00 +0100

    continue with probing qt.

--- src/GUI.py
+++ src/GUI.py
@@ -1,33 +1,58 @@
 #!/usr/bin/python
 
 # Sempel Source: http://www.saltycrane.com/blog/2008/01/pyqt-43-simple-qabstractlistmodel/
+# http://zetcode.com/tutorials/pyqt4/german/menusandtoolbars/
+# http://stackoverflow.com/questions/1100775/create-pyqt-menu-from-a-list-of-strings
 
 import sys
 from PyQt4.QtCore import * 
 from PyQt4.QtGui import * 
+from PyQt4 import QtGui
+
 
 #################################################################### 
 def main(): 
     app = QApplication(sys.argv) 
-    w = MyWindow() 
+    w = MainWindow() 
     w.show() 
     sys.exit(app.exec_()) 
 
 #################################################################### 
-class MyWindow(QWidget): 
+class MainWindow(QtGui.QMainWindow): 
     def __init__(self, *args): 
         QWidget.__init__(self, *args) 
 
-        # create table
-        list_data = [1,2,3,4]
-        lm = MyListModel(list_data, self)
-        lv = QListView()
-        lv.setModel(lm)
+        # pseudo data
+        _list_data = [1,2,3,4]
+        _lm = MyListModel(_list_data, self)
 
-        # layout
-        layout = QVBoxLayout()
-        layout.addWidget(lv) 
-        self.setLayout(layout)
+
+        # Main layout
+        _hMainLayout = QHBoxLayout()
+        self.setCentralWidget(_hMainLayout)
+
+	# VBox left
+        _vListLayoutL = QVBoxLayout()
+        _hMainLayout.addLayout(_vListLayoutL) 
+
+        # create table
+        _listview = QListView()
+        _listview.setModel(_lm)
+        _vListLayoutL.addWidget(_listview)
+
+
+	# VBox Right
+        _vListLayoutR = QVBoxLayout()
+        _hMainLayout.addLayout(_vListLayoutR) 
+
+        # create Rigth table
+        _listviewR = QListView()
+        _listviewR.setModel(_lm)
+        _vListLayoutR.addWidget(_listviewR)
+
+        # Statusbar
+        #self.statusBar().showMessage('Ready')
+
 
 #################################################################### 
 class MyListModel(QAbstractListModel): 
