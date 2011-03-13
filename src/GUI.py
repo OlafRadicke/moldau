db5f1619d3b04a38de8c993e58aa1acf05b5fc88
Olaf Radicke <briefkasten@olaf-radicke.de>
2011-03-13 21:20:05 +0100

    continue with probing pyqt.

--- src/GUI.py
+++ src/GUI.py
@@ -5,72 +5,96 @@
 # http://stackoverflow.com/questions/1100775/create-pyqt-menu-from-a-list-of-strings
 
 import sys
-from PyQt4.QtCore import * 
-from PyQt4.QtGui import * 
-from PyQt4 import QtGui
+#from PyQt4.QtCore import * 
+#from PyQt4.QtGui import * 
+#from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
 #################################################################### 
 def main(): 
-    app = QApplication(sys.argv) 
-    w = MainWindow() 
+    app = QtGui.QApplication(sys.argv)
+    w = MainWindow()
     w.show() 
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
 
-#################################################################### 
-class MainWindow(QtGui.QMainWindow): 
+##################################################################
+class MainWindow(QtGui.QMainWindow):
+
+    ## pseudo data
+    list_data  = [1,2,3,4]
+
+    ## Constructor
     def __init__(self, *args): 
-        QWidget.__init__(self, *args) 
+        QtGui.QWidget.__init__(self, *args) 
 
         # pseudo data
-        _list_data = [1,2,3,4]
-        _lm = MyListModel(_list_data, self)
+        lm = MyListModel(self.list_data, self)
 
+        ## menubar
+        exit = QtGui.QAction(QtGui.QIcon('icons/exit.png'), 'Exit', self)
+        exit.setShortcut('Ctrl+Q')
+        exit.setStatusTip('Exit application')
+        self.connect(exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+
+
+        
+        menubar = self.menuBar()
+        file = menubar.addMenu('&File')
+        file.addAction(exit)
+
+
+
+        # Main Widget
+        centralWidget = QtGui.QWidget()
+        self.setCentralWidget(centralWidget)
 
         # Main layout
-        _hMainLayout = QHBoxLayout()
-        self.setCentralWidget(_hMainLayout)
+        hMainLayout = QtGui.QHBoxLayout()
+        centralWidget.setLayout(hMainLayout)
 
 	# VBox left
-        _vListLayoutL = QVBoxLayout()
-        _hMainLayout.addLayout(_vListLayoutL) 
+        vListLayoutL = QtGui.QVBoxLayout()
+        hMainLayout.addLayout(vListLayoutL)
 
         # create table
-        _listview = QListView()
-        _listview.setModel(_lm)
-        _vListLayoutL.addWidget(_listview)
+        listview = QtGui.QListView()
+        listview.setModel(lm)
+        vListLayoutL.addWidget(listview)
 
 
 	# VBox Right
-        _vListLayoutR = QVBoxLayout()
-        _hMainLayout.addLayout(_vListLayoutR) 
+        vListLayoutR = QtGui.QVBoxLayout()
+        hMainLayout.addLayout(vListLayoutR)
 
         # create Rigth table
-        _listviewR = QListView()
-        _listviewR.setModel(_lm)
-        _vListLayoutR.addWidget(_listviewR)
+        listviewR = QtGui.QListView()
+        listviewR.setModel(lm)
+        vListLayoutR.addWidget(listviewR)
 
         # Statusbar
-        #self.statusBar().showMessage('Ready')
+        self.statusBar().showMessage('Ready')
 
 
 #################################################################### 
-class MyListModel(QAbstractListModel): 
+class MyListModel(QtCore.QAbstractListModel):
     def __init__(self, datain, parent=None, *args): 
         """ datain: a list where each item is a row
         """
-        QAbstractListModel.__init__(self, parent, *args) 
+        QtCore.QAbstractListModel.__init__(self, parent, *args)
         self.listdata = datain
  
-    def rowCount(self, parent=QModelIndex()): 
+    def rowCount(self, parent=QtCore.QModelIndex()): 
         return len(self.listdata) 
  
     def data(self, index, role): 
-        if index.isValid() and role == Qt.DisplayRole:
-            return QVariant(self.listdata[index.row()])
+        if index.isValid() and role == QtCore.Qt.DisplayRole:
+            return QtCore.QVariant(self.listdata[index.row()])
         else: 
-            return QVariant()
+            return QtCore.QVariant()
 
 ####################################################################
-if __name__ == "__main__": 
-    main()
+
+
+main()
